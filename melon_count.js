17b27bfe87a5c9fadfa22eca86227ec6882c3c5a
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-18 19:43:33 -0700

    Completed melon_counts.js

---
+++ melon_count.js
@@ -0,0 +1,33 @@
+
+// Our customers are going to buy lots of melons!
+
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+
+function count_melons(melonsArray) {
+    // Take in a list and return a dictionary of # of melons by melon type.
+
+    const melonCounts = {};
+
+    for (const melon of  melonsArray) {
+        if (melon in melonCounts) {
+            melonCounts[melon] += 1;
+        }
+        else {
+            melonCounts[melon] = 1;
+        }
+    }
+
+    return melonCounts;
+
+}
